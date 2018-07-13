b8f909ae4ff722ce2ae4eefb67150bda651b15f0
Dat <tdat.nguyen93@gmail.com>
2018-07-12 23:58:01 -0700

    Updated Modular Receiver

--- modular_receiver.py
+++ modular_receiver.py
@@ -6,7 +6,7 @@ class ModularReceiver(object):
     """Simulate a two parts: Demod and Decoder."""
 
     def __init__(self, num_classes, demod_model_path):
-        self.demod_model = self._demod_model_def(num_classes, demod_model_path)
+        self.demod_model = tf.keras.models.load_model(demod_model_path)
         # self.rnn_model = tf.keras.models.load_model(decoder_model_path)
 
     def demodulate(self, complex_inputs):
@@ -24,19 +24,3 @@ class ModularReceiver(object):
                       axis=-1)
         preprocessed_inputs = x.reshape((-1, 2))
         return preprocessed_inputs
-
-    def _demod_model_def(self, num_classes, model_path):
-        inputs  = Input(shape=(2,))
-        hiddens = Dense(256, use_bias=True, activation='selu')(inputs)
-        hiddens = Dense(256, use_bias=True, activation='selu')(inputs)
-        outputs = Dense(num_classes, use_bias=True, activation='softmax')(hiddens)
-        model = tf.keras.Model(inputs, outputs)
-
-        try:
-            model.load_weights(model_path)
-            print('Pre-trained weighs are loaded.')
-        except:
-            print('Cannot load demod model weights. Pass')
-
-
-        return model
